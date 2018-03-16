70b706bb7347e077f2845008602e8e237cc14fff
thetriplet3 <thetriplet3@gmail.com>
2018-03-17 01:14:30 +0530

    Implementation -Added a DB handler -Created schema for SMSLogs.

---
+++ app/src/main/java/com/sliit/yashstar/buzzapp/BuzzDBHandler.java
@@ -0,0 +1,40 @@
+package com.sliit.yashstar.buzzapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Tharushan on 2018-03-17.
+ */
+
+public class BuzzDBHandler extends SQLiteOpenHelper{
+    private static final String DB_NAME = "BuzzApp.db";
+
+    private static final String SQL_CREATE_SMSLOG_TAB =
+            "CREATE TABLE " + BuzzDBSchema.SMSLog.TABLE_NAME + " (" +
+                                BuzzDBSchema.SMSLog._ID + " INTEGER PRIMARY KEY," +
+                                BuzzDBSchema.SMSLog.COL_LOG_TIMESTAMP + " TIMESTAMP," +
+                                BuzzDBSchema.SMSLog.COL_SMS_MESSAGE + " TEXT, "+
+                                BuzzDBSchema.SMSLog.COL_SMS_NUMBER + " TEXT, "+
+                                BuzzDBSchema.SMSLog.COL_SMS_SENT_STATUS + " TEXT, "+
+                                BuzzDBSchema.SMSLog.COL_SMS_DELIVERY_STATUS + " TEXT "+
+                    ")";
+    private static final String SQL_DELETE_SMSLOG_TAB =
+            "DROP TABLE IF EXISTS " + BuzzDBSchema.SMSLog.TABLE_NAME ;
+
+    public BuzzDBHandler(Context context) {
+        super(context.getApplicationContext(), DB_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_SMSLOG_TAB);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE_SMSLOG_TAB);
+        onCreate(db);
+    }
+}
---
+++ app/src/main/java/com/sliit/yashstar/buzzapp/BuzzDBSchema.java
@@ -0,0 +1,20 @@
+package com.sliit.yashstar.buzzapp;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Tharushan on 2018-03-17.
+ */
+
+public final class BuzzDBSchema {
+    private BuzzDBSchema(){}
+
+    public static class SMSLog implements BaseColumns {
+        public static final String TABLE_NAME = "SMSLog";
+        public static final String COL_LOG_TIMESTAMP = "LogTimeStamp";
+        public static final String COL_SMS_MESSAGE = "LogMessage";
+        public static final String COL_SMS_NUMBER = "LogNumber";
+        public static final String COL_SMS_SENT_STATUS = "LogSMSSentStatus";
+        public static final String COL_SMS_DELIVERY_STATUS = "LogSMSDeliveryStatus";
+    }
+}
